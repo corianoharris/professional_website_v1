8053964c89718fc8fa4961cbfd5875369e33acd3
Coriano Harris <corianoharrispro@gmail.com>
2025-12-30 10:52:48 -0600

    removed v0 icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,19 +55,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
         url: "/favicon.svg",
         type: "image/svg+xml",
       },
+      {
+        url: "/my_logo.svg",
+        type: "image/svg+xml",
+      },
     ],
-    apple: "/apple-icon.png",
+    apple: "/my_logo.svg",
   },
   appleWebApp: {
     capable: true,
